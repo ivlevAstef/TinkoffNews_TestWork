9b8a777e23e4f94281821e4e840cbd5d75c07c96
Ivlev Alexander <ivlev.stef@gmail.com>
2017-09-08 23:19:12 +0700

    empty project with correct settings

---
+++ Sources/Code/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TinkoffNews_TestWork
+//
+//  Created by Alexander Ivlev on 08/09/2017.
+//  Copyright © 2017 SIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
+
